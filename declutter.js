7f42219c302b0c16f47d9b2d93da23208d48b7fb
Aman Garg <amangargcse@outlook.com>
2021-02-12 04:02:26 +0530

    Declutter stuff

---
+++ declutter.js
@@ -0,0 +1,14 @@
+let color = '#ddd';
+
+window.onload = removeStickyHeader;
+
+function removeStickyHeader() {
+  // Remove the sticky
+  document.querySelectorAll(".site-header").forEach(el => el.remove());
+  
+  // Remove the sidebar
+  document.querySelectorAll(".td-pb-span4.td-main-sidebar").forEach(el => el.remove());
+  
+  // Center the main content
+  document.querySelectorAll('.td-pb-span8.td-main-content').forEach(el => el.style.marginLeft = "17%");
+};
